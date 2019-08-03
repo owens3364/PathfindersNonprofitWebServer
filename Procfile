ccbfaf60501f14dfd79bdd77728276de84acd613
Owen Scott <owens3364@gmail.com>
2019-08-03 15:10:40 -0500

    Apple Pay Verification Enabled

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: bin/org.pathfindersrobotics.server
--- main.go
+++ main.go
@@ -2,11 +2,17 @@ package main
 
 import (
 "github.com/gin-gonic/gin"
+"github.com/gin-contrib/gzip"
 "github.com/gin-contrib/static"
+"log"
+"net/http"
+"os"
 )
 
 func main() {
+	gin.SetMode(gin.ReleaseMode)
 	router := gin.Default()
+	router.Use(gzip.Gzip(gzip.DefaultCompression))
 	router.Use(static.Serve("/", static.LocalFile("./static", true)))
-	router.Run(":443")
+	log.Fatal(http.ListenAndServe(":" + os.Getenv("PORT"), router))
 }
