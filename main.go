c44ec45eb7dce7e275b8019b3dc2b090edcbb3f6
Owen Scott <owens3364@gmail.com>
2019-08-04 18:49:13 -0500

    Added HTTPS redirection but actually built the binary for it this time

--- main.go
+++ main.go
@@ -14,8 +14,8 @@ func main() {
 
 	httpsRedirectRouter := gin.Default()
 	httpsRedirectRouter.GET("/*anything", func(c *gin.Context) {
-		c.Redirect(301, "https://www.pathfindersrobotics.org/" + c.Param(anything))
-	}
+		c.Redirect(301, "https://www.pathfindersrobotics.org/" + c.Param("anything"))
+	})
 	go log.Fatal(http.ListenAndServe(":" + os.Getenv("HTTP_PORT"), httpsRedirectRouter))
 
 	router := gin.Default()
