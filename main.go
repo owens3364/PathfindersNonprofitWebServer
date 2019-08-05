277f67ecdaf0ccebfa762f07dc9475d0bae5342f
Owen Scott <owens3364@gmail.com>
2019-08-04 22:43:08 -0500

    Using new security middleware

--- main.go
+++ main.go
@@ -4,22 +4,28 @@ import (
 "github.com/gin-gonic/gin"
 "github.com/gin-contrib/gzip"
 "github.com/gin-contrib/static"
+"github.com/unrolled/secure"
 "log"
 "net/http"
 "os"
 )
 
-func main() {
-	//httpsRedirectRouter := gin.New()
-	//httpsRedirectRouter.GET("/*anything", func(c *gin.Context) {
-	//	c.Redirect(301, "https://www.pathfindersrobotics.org/" + c.Param("anything"))
-	//})
-	//go log.Fatal(http.ListenAndServe(":" + os.Getenv("HTTP_PORT"), httpsRedirectRouter))
-	//go httpsRedirectRouter.Run(":" + os.Getenv("HTTP_PORT"))
-
+func main(){
 	router := gin.Default()
+	router.Use(func() gin.HandlerFunc {
+                return func(c *gin.Context) {
+                        err := secure.New(secure.Options {
+                                SSLRedirect: true,
+                        }).Process(c.Writer, c.Request)
+                        if err != nil {
+                                log.Println(err)
+                                return
+                        }
+                        c.Next()
+                }
+        }())
 	router.Use(gzip.Gzip(gzip.DefaultCompression))
 	router.Use(static.Serve("/", static.LocalFile("./static", true)))
 
-	log.Fatal(http.ListenAndServe(":" + os.Getenv("HTTPS_PORT"), router))
+	log.Fatal(http.ListenAndServe(":" + os.Getenv("PORT"), router))
 }
