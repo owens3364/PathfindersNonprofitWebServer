384fa1897f1f76822e214e6e0105222cf99762f6
Owen Scott <owens3364@gmail.com>
2019-08-18 22:24:41 -0500

    Major update to email receipts and automated HTML emailing

--- main.go
+++ main.go
@@ -47,6 +47,47 @@ type Token struct {
 	StripeToken *string `form:"token" json:"token" binding:"exists"`
 }
 
+type EmailData struct {
+	DonorInformation PaymentData
+
+	TeamEmail                string
+	Team                     string
+	FIRSTSuffix              string
+	PRAddr1                  string
+	PRCity                   string
+	PRState                  string
+	PRZip                    string
+	PRPhone                  string
+	EIN                      string
+	Date                     string
+	CurrentSeason            string
+	WebServerEmail           string
+	WebServerPassword        string
+	DonationReceiptsEmail    string
+	DonationReceiptsPassword string
+	ServerAddress            string
+	ServerPort               string
+}
+
+type osEnvVarError struct {
+	errorMessage string
+}
+
+func (errType *osEnvVarError) Error() string {
+	return errType.errorMessage
+}
+
+const FTCPathfinders13497 string = "FTC Pathfinders 13497"
+const FLLPhoenixVoyagers7885 string = "FLL Phoenix Voyagers 7885"
+
+const Email13497 string = "ftc13497@pathfindersrobotics.org"
+const Email7885 string = "fll7885@pathfindersrobotics.org"
+
+const FTCSuffix string = "Tech Challenge"
+const FLLSuffix string = "Lego League"
+
+const EmailFinance string = "finance@pathfindersrobotics.org"
+
 func main() {
 	router := gin.Default()
 	fmt.Println("Router instance created")
@@ -266,46 +307,176 @@ func tokenToPaymentData(pre *Token) *PaymentData {
 }
 
 func sendPaymentEmail(data *PaymentData) {
-	to := determineTeamEmail(data)
-	if to != "" {
-		username := os.Getenv("WEBSERVER_EMAIL_USERNAME")
-		if username == "" {
-			fmt.Println("ERROR: 'WEBSERVER_EMAIL_USERNAME' ENVIRONMENT VARIABLE UNAVAILABLE")
-			return
+	htmlEmail := "<!DOCTYPE html PUBLIC \"-//W3C//DTD XHTML 1.0 Transitional//EN\" \"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd\"><html xmlns=\"http://www.w3.org/1999/xhtml\" lang=\"en\"><head><meta charset=\"utf-8\" /><meta name=\"viewport\" content=\"width=device-width, initial-scale=1, shrink-to-fit=no, viewport-fit=cover\"/><html dir=\"ltr\"></html><meta http-equiv=\"Content-Type\" content=\"text/html; charset=UTF-8\" /><title>Pathfinders Robotics Donation Receipt</title><meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\"/><meta http-equiv=\"X-UA-Compatible\" content=\"IE=7\" /><meta http-equiv=\"X-UA-Compatible\" content=\"IE=8\" /><meta http-equiv=\"X-UA-Compatible\" content=\"IE=9\" /><!--[if !mso]><!-- --><meta http-equiv=\"X-UA-Compatible\" content=\"IE=edge\" /><!--<![endif]--></head><body style=\"margin: 0; padding: 0; font-family: 'Times New Roman', Times, serif; letter-spacing: 0em;\"><table border=\"0\" cellpadding=\"0\" cellspacing=\"0\" width=\"100%\"  style=\"font-size: 12pt;\"><tr><td style=\"width: 50%;\"><img src=\"https://www.pathfindersrobotics.org/assets/receipts/Logo.png\" alt=\"Pathfinders Robotics\" width=\"265\" border=\"0\" style=\"display: block; height: auto;\" /></td><td style=\"width: 50%; text-align: right;\">Pathfinders Robotics<br/>${PRAddr1}, ${PRCity}, ${PRState} ${PRZip}<br/>${PRPhone}&nbsp;</td></tr></table><table border=\"0\" cellpadding=\"0\" cellspacing=\"0\" width=\"100%\" style=\"border-bottom: 2px solid black; font-size: 14pt;\"><tr><td>${Date}<br/><br/>${Name}<br/>${Addr1}${Addr2}<br/>${City}, ${State} ${Zip}<br/><br/>Thank you so much for your very generous donation of $${Amount} to the Pathfinders Robotics organization received on ${Date}.<br/><br/>Your donation will help us in supporting ${Team} in FIRST® ${FIRSTSuffix}. We will proudly include ${Name} information and branding during the ${CurrentSeason} FIRST® ${FIRSTSuffix} season.<br/><br/>Thanks again for your generosity and support.<br/><br/>Respectfully,<img src=\"https://www.pathfindersrobotics.org/assets/receipts/Signature.png\" alt=\"Bhooshan Karnik\" width=\"160\" border=\"0\" style=\"display: block; height: auto;\" /><br/>Bhooshan Karnik<br/>Treasurer of Pathfinders Robotics<br/><br/><br/></td></tr></table><br/><table border=\"0\" cellpadding=\"0\" cellspacing=\"0\" width=\"100%\"><tr><td style=\"text-align: center; font-size: 11pt;\"><b>Donation receipt</b> - Keep for your records</td></tr><tr><td style=\"font-size: 13pt;\">Donor: ${Name}<br/>Date Received: ${Date}<br/>Cash Contribution: $${Amount}<br/><br/>Pathfinders Robotics<br/>${PRAddr1}<br/>${PRCity}, ${PRState} ${PRZip}<br/>Federal Tax ID ${EIN}</td></tr></table></body></html>"
+
+	emailData, err := genEmailData(*data)
+	if err != nil {
+		notifBody := "To: " + emailData.TeamEmail + "\r\nSubject: New Payment\r\n\r\nNew Payment\r\nAmount: " + strconv.Itoa(*data.Amount) + "\r\nDescription: " + *data.Description + "\r\nName: " + *data.Name + "\r\nAddr1: " + *data.Addr1 + "\r\nAddr2: " + *data.Addr2 + "\r\nCity: " + *data.City + "\r\nState: " + *data.State + "\r\nZip: " + *data.Zip + "\r\nEmail: " + *data.Email + "\r\nPhone: " + *data.Phone
+		notifAuth := smtp.PlainAuth("", emailData.WebServerEmail, emailData.WebServerPassword, emailData.ServerAddress)
+		notifErr := smtp.SendMail(emailData.ServerAddress+":"+emailData.ServerPort, notifAuth, emailData.WebServerEmail, []string{emailData.TeamEmail, EmailFinance}, []byte(notifBody))
+		if notifErr != nil {
+			fmt.Println(notifErr)
+			fmt.Println("ERROR: NOTIFICATION EMAIL TO TEAM AND FINANCE (BCC) COULD NOT BE SENT")
 		}
-		password := os.Getenv("WEBSERVER_EMAIL_PASSWORD")
-		if password == "" {
-			fmt.Println("ERROR: 'WEBSERVER_EMAIL_PASSWORD' ENVIRONMENT VARIABLE UNAVAILABLE")
-			return
-		}
-		serverAddress := os.Getenv("SmtpServerAddress")
-		if serverAddress == "" {
-			fmt.Println("ERROR: 'SmtpServerAddress' ENVIRONMENT VARIABLE UNAVAILABLE")
-			return
-		}
-		serverPort := os.Getenv("SmtpServerPort")
-		if serverPort == "" {
-			fmt.Println("ERROR: 'serverPort' ENVIRONMENT VARIABLE UNAVAILABLE")
-			return
-		}
-		body := "To: " + to + ", finance@pathfindersrobotics.org\r\nSubject: New Payment\r\n\r\nNew Payment\r\nAmount: " + strconv.FormatInt(int64(*data.Amount), 10) + "\r\nDescription: " + *data.Description + "\r\nName: " + *data.Name + "\r\nAddr1: " + *data.Addr1 + "\r\nAddr2: " + *data.Addr2 + "\r\nCity: " + *data.City + "\r\nState: " + *data.State + "\r\nZip: " + *data.Zip + "\r\nEmail: " + *data.Email + "\r\nPhone: " + *data.Phone
-		auth := smtp.PlainAuth("", username, password, serverAddress)
-		err := smtp.SendMail(serverAddress+":"+serverPort, auth, username, []string{to, "finance@pathfindersrobotics.org"}, []byte(body))
-		if err != nil {
-			fmt.Println(err)
-			fmt.Println("EMAIL COULD NOT BE SENT")
+
+		htmlEmail = strings.ReplaceAll(htmlEmail, "${PRAddr1}", emailData.PRAddr1)
+		htmlEmail = strings.ReplaceAll(htmlEmail, "${PRCity}", emailData.PRCity)
+		htmlEmail = strings.ReplaceAll(htmlEmail, "${PRState}", emailData.PRState)
+		htmlEmail = strings.ReplaceAll(htmlEmail, "${PRZip}", emailData.PRZip)
+		htmlEmail = strings.ReplaceAll(htmlEmail, "${PRPhone}", emailData.PRPhone)
+		htmlEmail = strings.ReplaceAll(htmlEmail, "${Date}", emailData.Date)
+		htmlEmail = strings.ReplaceAll(htmlEmail, "${Name}", *emailData.DonorInformation.Name)
+		htmlEmail = strings.ReplaceAll(htmlEmail, "${Addr1}", *emailData.DonorInformation.Addr1)
+		htmlEmail = strings.ReplaceAll(htmlEmail, "${Addr2}", *emailData.DonorInformation.Addr2)
+		htmlEmail = strings.ReplaceAll(htmlEmail, "${City}", *emailData.DonorInformation.City)
+		htmlEmail = strings.ReplaceAll(htmlEmail, "${State}", *emailData.DonorInformation.State)
+		htmlEmail = strings.ReplaceAll(htmlEmail, "${Zip}", *emailData.DonorInformation.Zip)
+		htmlEmail = strings.ReplaceAll(htmlEmail, "${Amount}", fmt.Sprintf("%g", float64(*emailData.DonorInformation.Amount)/100.0))
+		htmlEmail = strings.ReplaceAll(htmlEmail, "${Team}", emailData.Team)
+		htmlEmail = strings.ReplaceAll(htmlEmail, "${FIRSTSuffix}", emailData.FIRSTSuffix)
+		htmlEmail = strings.ReplaceAll(htmlEmail, "${CurrentSeason}", emailData.CurrentSeason)
+		htmlEmail = strings.ReplaceAll(htmlEmail, "${EIN}", emailData.EIN)
+
+		receiptBody := "To: " + emailData.DonationReceiptsEmail + "\r\nSubject: Pathfinders Robotics Donation Receipt\r\n" + htmlEmail
+		receiptAuth := smtp.PlainAuth("", emailData.DonationReceiptsEmail, emailData.DonationReceiptsPassword, emailData.ServerAddress)
+		receiptErr := smtp.SendMail(emailData.ServerAddress+":"+emailData.ServerPort, receiptAuth, emailData.DonationReceiptsEmail, []string{*emailData.DonorInformation.Email, emailData.TeamEmail, EmailFinance}, []byte(receiptBody))
+		if receiptErr != nil {
+			fmt.Println(receiptErr)
+			fmt.Println("ERROR: RECEIPT EMAIL TO DONOR AND TEAM (BCC) AND FINANCE(BCC) COULD NOT BE SENT")
 		}
 	} else {
-		fmt.Println("ERROR: TEAM EMAIL FROM DESCRIPTION INVALID")
+		fmt.Println("ERROR: EMAIL COULD NOT BE GENERATED OR DELIVERED")
 	}
 }
 
-func determineTeamEmail(data *PaymentData) string {
-	if strings.Contains(*data.Description, "FTC Pathfinders 13497") {
-		return "ftc13497@pathfindersrobotics.org"
+func genEmailData(origin PaymentData) (EmailData, error) {
+	teamEmail, team, firstSuffix := determineTeamEmail(&origin)
+
+	if teamEmail == "" || team == "" || firstSuffix == "" {
+		err := "ERROR: FIELD 'DESCRIPTION' IN TYPE 'PaymentData' NOT RECOGNIZED"
+		fmt.Println(err)
+		return EmailData{}, &osEnvVarError{err}
 	}
-	if strings.Contains(*data.Description, "FLL Phoenix Voyagers 7885") {
-		return "fll7885@pathfindersrobotics.org"
+
+	prAddr1 := os.Getenv("PRAddr1")
+	if prAddr1 == "" {
+		err := "ERROR: 'PRAddr1' ENVIRONMENT VARIABLE UNAVAILABLE"
+		fmt.Println(err)
+		return EmailData{}, &osEnvVarError{err}
 	}
-	return ""
+	prCity := os.Getenv("PRCity")
+	if prCity == "" {
+		err := "ERROR: 'PRCity' ENVIRONMENT VARIABLE UNAVAILABLE"
+		fmt.Println(err)
+		return EmailData{}, &osEnvVarError{err}
+	}
+	prState := os.Getenv("PRState")
+	if prState == "" {
+		err := "ERROR: 'PRState' ENVIRONMENT VARIABLE UNAVAILABLE"
+		fmt.Println(err)
+		return EmailData{}, &osEnvVarError{err}
+	}
+	prZip := os.Getenv("PRZip")
+	if prZip == "" {
+		err := "ERROR: 'PRZip' ENVIRONMENT VARIABLE UNAVAILABLE"
+		fmt.Println(err)
+		return EmailData{}, &osEnvVarError{err}
+	}
+	prPhone := os.Getenv("PRPhone")
+	if prPhone == "" {
+		err := "ERROR: 'PRPhone' ENVIRONMENT VARIABLE UNAVAILABLE"
+		fmt.Println(err)
+		return EmailData{}, &osEnvVarError{err}
+	}
+
+	ein := os.Getenv("EIN")
+	if ein == "" {
+		err := "ERROR: 'EIN' ENVIRONMENT VARIABLE UNAVAILABLE"
+		fmt.Println(err)
+		return EmailData{}, &osEnvVarError{err}
+	}
+
+	est, _ := time.LoadLocation("EST")
+	currentTime := time.Now().In(est)
+	month := currentTime.Month().String()
+	day := currentTime.Day()
+	year := currentTime.Year()
+	date := month + " " + strconv.Itoa(day) + ", " + strconv.Itoa(year)
+
+	currentSeason := ""
+	if currentTime.Month() < 5 {
+		currentSeason = strconv.Itoa(year-1) + "-" + strconv.Itoa(year)
+	} else {
+		currentSeason = strconv.Itoa(year) + "-" + strconv.Itoa(year+1)
+	}
+
+	webserverUsr := os.Getenv("WEBSERVER_EMAIL_USERNAME")
+	if webserverUsr == "" {
+		err := "ERROR: 'WEBSERVER_EMAIL_USERNAME' ENVIRONMENT VARIABLE UNAVAILABLE"
+		fmt.Println(err)
+		return EmailData{}, &osEnvVarError{err}
+	}
+	webserverPwd := os.Getenv("WEBSERVER_EMAIL_PASSWORD")
+	if webserverPwd == "" {
+		err := "ERROR: 'WEBSERVER_EMAIL_PASSWORD' ENVIRONMENT VARIABLE UNAVAILABLE"
+		fmt.Println(err)
+		return EmailData{}, &osEnvVarError{err}
+	}
+
+	donationReceiptsUsr := os.Getenv("DONATION_RECEIPTS_EMAIL_USERNAME")
+	if donationReceiptsUsr == "" {
+		err := "ERROR: 'DONATION_RECEIPTS_EMAIL_USERNAME' ENVIRONMENT VARIABLE UNAVAILABLE"
+		fmt.Println(err)
+		return EmailData{}, &osEnvVarError{err}
+	}
+	donationReceiptsPwd := os.Getenv("DONATION_RECEIPTS_EMAIL_PASSWORD")
+	if donationReceiptsPwd == "" {
+		err := "ERROR: 'DONATION_RECEIPTS_EMAIL_PASSWORD' ENVIRONMENT VARIABLE UNAVAILABLE"
+		fmt.Println(err)
+		return EmailData{}, &osEnvVarError{err}
+	}
+
+	serverAddress := os.Getenv("SmtpServerAddress")
+	if serverAddress == "" {
+		err := "ERROR: 'SmtpServerAddress' ENVIRONMENT VARIABLE UNAVAILABLE"
+		fmt.Println(err)
+		return EmailData{}, &osEnvVarError{err}
+	}
+	serverPort := os.Getenv("SmtpServerPort")
+	if serverPort == "" {
+		err := "ERROR: 'serverPort' ENVIRONMENT VARIABLE UNAVAILABLE"
+		fmt.Println(err)
+		return EmailData{}, &osEnvVarError{err}
+	}
+
+	return EmailData{
+		DonorInformation:         origin,
+		TeamEmail:                teamEmail,
+		Team:                     team,
+		FIRSTSuffix:              firstSuffix,
+		PRAddr1:                  prAddr1,
+		PRCity:                   prCity,
+		PRState:                  prState,
+		PRZip:                    prZip,
+		PRPhone:                  prPhone,
+		EIN:                      ein,
+		Date:                     date,
+		CurrentSeason:            currentSeason,
+		WebServerEmail:           webserverUsr,
+		WebServerPassword:        webserverPwd,
+		DonationReceiptsEmail:    donationReceiptsUsr,
+		DonationReceiptsPassword: donationReceiptsPwd,
+		ServerAddress:            serverAddress,
+		ServerPort:               serverPort,
+	}, nil
+}
+
+func determineTeamEmail(data *PaymentData) (string, string, string) {
+	if strings.Contains(*data.Description, FTCPathfinders13497) {
+		return Email13497, FTCPathfinders13497, FTCSuffix
+	}
+	if strings.Contains(*data.Description, FLLPhoenixVoyagers7885) {
+		return Email7885, FLLPhoenixVoyagers7885, FLLSuffix
+	}
+	return "", "", ""
 }
